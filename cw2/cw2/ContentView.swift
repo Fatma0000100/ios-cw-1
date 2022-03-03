49a22a5cefcc9f7da3801c2913c101aa895b0c40
fatma0000100 <frmr2005@gmail.com>
2022-03-02 20:49:11 -0800

    done

---
+++ cw2/cw2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cw2
+//
+//  Created by mac on 02/03/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("hy my name is fatma")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cw2/cw2/cw2App.swift
@@ -0,0 +1,17 @@
+//
+//  cw2App.swift
+//  cw2
+//
+//  Created by mac on 02/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct cw2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
